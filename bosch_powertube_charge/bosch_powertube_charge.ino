fd3f6fbda8008a4a3ca11001038d5d3432d8f753
HerrStrobl <lukas@herrstrobl.de>
2021-11-08 12:06:28 +0100

    working so far

--- bosch_powertube_charge/bosch_powertube_charge.ino
+++ bosch_powertube_charge/bosch_powertube_charge.ino
@@ -1,7 +1,11 @@
 #include <Wire.h> // Wire Bibliothek einbinden
 #include <LiquidCrystal_I2C.h> // Vorher hinzugefügte LiquidCrystal_I2C Bibliothek einbinden
 
-#define SLEEP_TIME 500
+#define SLEEP_TIME 50
+#define BUTTON_LONGPRESS_TIME 2000
+
+#define ASCII_NUMBER '0'
+#define fTS_LENGTH 20
 
 #define LCD_DISPLAY_ADDR 0x27
 #define LCD_DISPLAY_LEN 16
@@ -18,26 +22,87 @@
 #define PIN_STEPS 1024.0
 #define VOLTAGE_DIVIDER 10.0
 
+#define STATE_INITIAL           0b0000
+#define STATE_BATTERY_CONNECTED 0b0001
+#define STATE_BATTERY_CHARGING  0b0010
+#define STATE_BATTERY_CHARGED   0b0100
+#define STATE_BATTERY_ERROR     0b1000
+
+//                          0123456789abcdef
+#define TEXT_CONNECT_BAT   "Connect Battery "
+#define TEXT_BAT_CONNECTED "Connected       "
+#define TEXT_BAT_CHARGING  "Charging        "
+#define TEXT_BAT_CHARGED   "Battery charged "
+#define TEXT_BAT_ERROR     "ERROR           "
+
+#define MOVE_DOT_LCD_POS 8
+#define MOVE_DOT_CNT 8
+
+#define CELL_PERCENTAGE_MAX 0.8
+
+const uint32_t LOOP_MASK_all    = 0b11111111111111111111111111111111;
+const uint32_t LOOP_MASK_half   = 0b01010101010101010101010101010101;
+const uint32_t LOOP_MASK_4th    = 0b00010001000100010001000100010001;
+const uint32_t LOOP_MASK_8th    = 0b00000001000000010000000100000001;
+const uint32_t LOOP_MASK_16th   = 0b00000000000000010000000000000001;
+const uint32_t LOOP_MASK_32th   = 0b00000000000000000000000000000001;
+
+typedef unsigned int state;
+typedef unsigned long time_t;
+
 LiquidCrystal_I2C lcd(LCD_DISPLAY_ADDR, LCD_DISPLAY_LEN, LCD_DISPLAY_LINES);
 
-volatile bool bButtonPressed = false;
-volatile float dVoltageMeasured = 0;
-int nVoltageMeasured_mV;
-int nVoltageMeasured_V_pre;
-int nVoltageMeasured_V_after;
-int nBatteryVoltage_mV;
-int nBatteryVoltage_V_pre;
-int nBatteryVoltage_V_after;
+volatile state gState = STATE_INITIAL;
 
-const int nBatteryLevel[] = {0.04, 0.06, 0.08, 0.10, 0.12, 0.16, 0.2, 0.3, 0.4, 0.52, 0.64, 0.74, 0.83, 0.96, 1};
+volatile time_t t_startup;
+volatile time_t t_loop;
+volatile time_t t_loop_last = 0;
+volatile long t_loopdelta = SLEEP_TIME;
+volatile long t_loopdelta_corrector = 0;
+volatile time_t t_sleep_time = SLEEP_TIME;
+volatile time_t t_charge_begin;
+volatile time_t t_charge_end;
+volatile time_t t_charge_total = 0;
+volatile time_t t_button_pressed = 0;
+
+int move_dot_pos = 0;
+
+volatile bool bButtonPressed_last = false;
+volatile bool bButtonPressed = false;
+volatile bool bButtonPressed_short = false;
+volatile bool bButtonPressed_long = false;
+volatile float dVoltageMeasured = 0;
+volatile float dVoltageMeasured_smoothed = 0;
+volatile float fBatteryVoltage_V = 0;
+volatile float fCellPercentage = 0;
+
+const float fBatteryLevel[] = {0.04, 0.06, 0.08, 0.10, 0.12, 0.16, 0.2, 0.3, 0.4, 0.52, 0.64, 0.74, 0.83, 0.96, 1};
 const float fBatteryLevelVoltageOffset = 2.8;
-const int nBatteryLevelVoltageMultiplier = 10;
+const int fBatteryLevelVoltageMultiplier = 10;
 const int nMaxIndex = 14;
 
-float getBatteryLevel(float fVoltage);
+char * floatToString(char * cString, float fNumber, int nDigits);
+float getCellLevel(float fVoltage);
+void mngLoopTime();
+void wipeDisplayTop();
+void wipeDisplayBottom();
+void wipeDisplay();
+void startCharging();
+void stopCharging();
+bool battConnected();
+bool battCharged();
+bool consumeButtonShortPress();
+bool consumeButtonLongPress();
+void mgnButton();
+
+
+uint32_t _loop;
 
 void setup() 
 {
+Serial.begin(9600);
+Serial.print("Setup\n");
+
 lcd.init(); //Im Setup wird der LCD gestartet 
 lcd.backlight(); //Hintergrundbeleuchtung einschalten (lcd.noBacklight(); schaltet die Beleuchtung aus).
 
@@ -47,86 +112,403 @@ pinMode(PIN_LED_GREEN, OUTPUT);
 pinMode(PIN_LED_BLUE, OUTPUT);
 pinMode(PIN_BUTTON, INPUT);
 
+t_startup = millis();
+Serial.print("startup time: ");
+Serial.print(t_startup);
+Serial.print("\n");
+
+wipeDisplay();
+lcd.setCursor(0, 0);
+lcd.print(TEXT_CONNECT_BAT);
+_loop = 0b10000000;
 }
 
 void loop() 
 {
-Serial.begin(9600);
-char text[30]; 
+_loop = _loop << 1;
+if(_loop == 0) _loop = 1;
+
+Serial.print("Loop\n");
+Serial.print(_loop);
+Serial.print("\n");
+mngLoopTime();
+
+char cText_lcd[LCD_DISPLAY_LEN]; 
+char fTS_tmp[fTS_LENGTH];
 
 // Read Inputs
-bButtonPressed = digitalRead(PIN_BUTTON) == 0;
+mgnButton();
+
+if(_loop & LOOP_MASK_4th)
+{
 dVoltageMeasured = analogRead(PIN_MEAS_VOLTAGE) * REF_VOLTAGE / PIN_STEPS;
+dVoltageMeasured_smoothed = (dVoltageMeasured_smoothed + dVoltageMeasured * 49) / 50;
 
-float percentage = getBatteryLevel(dVoltageMeasured);
-int percentage_pre = (percentage*100);
-int percentage_past = (percentage*100-percentage_pre)*100;
-
-// CALC VOLTAGE
-//int nVoltageMeasured_mV = dVoltageMeasured * 1000;
-//int nVoltageMeasured_V_pre = dVoltageMeasured;
-//int nVoltageMeasured_V_after = (dVoltageMeasured - nVoltageMeasured_V_pre) * 100;
-int nBatteryVoltage_mV = nVoltageMeasured_mV * VOLTAGE_DIVIDER;
-int nBatteryVoltage_V_pre = dVoltageMeasured * VOLTAGE_DIVIDER;
-int nBatteryVoltage_V_after = (dVoltageMeasured * VOLTAGE_DIVIDER * 100) - (nBatteryVoltage_V_pre * 100);
+fBatteryVoltage_V = dVoltageMeasured * VOLTAGE_DIVIDER;
+fCellPercentage = getCellLevel(dVoltageMeasured);
 
 // WRITE VOLTAGE
-char sVoltMeas[30]; 
-//sprintf(sVoltMeas, "U=%d.%dV", nVoltageMeasured_V_pre, nVoltageMeasured_V_after);
-sprintf(sVoltMeas, "U=%d.%dV", nBatteryVoltage_V_pre, nBatteryVoltage_V_after);
-lcd.setCursor(8, 0);
-lcd.print(sVoltMeas);
+floatToString(fTS_tmp, fBatteryVoltage_V , 2);
+sprintf(cText_lcd, "%s   ", fTS_tmp);
+lcd.setCursor(0, 1);
+lcd.print(cText_lcd);
+lcd.setCursor(7, 1);
+lcd.print("V");
 
 // WRITE PERCENTAGE
-char sPercMeas[30]; 
-sprintf(sPercMeas, "%d.%d %%", percentage_pre, percentage_past);
-lcd.setCursor(8, 1);
-lcd.print(sPercMeas); 
-
-sprintf(text, "U = %d mV \n", nVoltageMeasured_mV);
-Serial.print(text);
-
-if(bButtonPressed)
-{
-  digitalWrite(PIN_RELAIS, HIGH);
+floatToString(fTS_tmp, fCellPercentage*100, 2);
+sprintf(cText_lcd, "%s   ", fTS_tmp);
+lcd.setCursor(9, 1);
+lcd.print(cText_lcd);
+lcd.setCursor(15, 1);
+lcd.print("%%");
 }
-else 
-{
-  digitalWrite(PIN_RELAIS, LOW);
-}
+
+// ---------- STATE MACHINE ---------- 
+  switch(gState)
+  {
+    case STATE_INITIAL:
+      if(battConnected()) switchState(STATE_BATTERY_CONNECTED);
+      break;
+      
+    case STATE_BATTERY_CONNECTED:
+      if(!battConnected())
+      {
+        switchState(STATE_INITIAL);
+        break;
+      }
+      if(battCharged())
+      {
+        switchState(STATE_BATTERY_CHARGED);
+        break;
+      }
+      if(bButtonPressed && !battCharged())
+      {
+        if(consumeButtonShortPress()) switchState(STATE_BATTERY_CHARGING);
+      }
+      break;
+      
+    case STATE_BATTERY_CHARGING:
+      if(!battConnected())
+      {
+        switchState(STATE_INITIAL);
+        break;
+      }
+      if(battCharged())
+      {
+        switchState(STATE_BATTERY_CHARGED);
+        break;
+      }
+      else if(bButtonPressed)
+      {
+        if(consumeButtonShortPress()) switchState(STATE_BATTERY_CONNECTED);
+      }
+
+      if(_loop & LOOP_MASK_16th)
+      {
+        char dots[MOVE_DOT_CNT] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        for(int i = 0; i < move_dot_pos; ++i)
+        {
+          dots[i] = '.';
+        }
+        move_dot_pos++;
+        if(move_dot_pos > MOVE_DOT_CNT) move_dot_pos = 1;
+        
+        lcd.setCursor(MOVE_DOT_LCD_POS, 0);
+        lcd.print(dots);
+      }
+      break;
+      
+    case STATE_BATTERY_CHARGED:
+      if(!battConnected())
+      {
+        switchState(STATE_INITIAL);
+        break;
+      }
+      if(!battCharged())
+      {
+        switchState(STATE_BATTERY_CONNECTED);
+        break;
+      }
+      break;
+      
+    case STATE_BATTERY_ERROR:
+      break;
+      
+    default:
+      break;
+  }
+
 digitalWrite(PIN_LED_RED, LOW);
 digitalWrite(PIN_LED_GREEN, HIGH);
 digitalWrite(PIN_LED_BLUE, LOW);
 
 //digitalRead(PIN_BUTTON);
 
-delay(SLEEP_TIME);  //500ms Pause
-
-//lcd.setCursor(0, 0);//Hier wird die Position des ersten Zeichens festgelegt. In diesem Fall bedeutet (0,0) das erste Zeichen in der ersten Zeile. 
-//lcd.print("Ich liebe dich !"); 
-//lcd.setCursor(0, 1);// In diesem Fall bedeutet (0,1) das erste Zeichen in der zweiten Zeile. 
-//lcd.print(" <3 <3 <3 <3 <3 ");
-
-
-Serial.print("test");
-sprintf(text, "U = %d.%dV \n", 41,5);
-Serial.print(text);
-sprintf(text, "Perc %d %% \n", (int) (getBatteryLevel(4.15)*100.0));
-Serial.print(text);
-
+delay(t_sleep_time);  //500ms Pause
 }
 
-float getBatteryLevel(float fVoltage)
+char * floatToString(char * cString, float fNumber, int nDigits)
+{
+  //char cString[20];
+  for(int n = 0; n < fTS_LENGTH; ++n)
+  {
+    cString[n] = 0;
+  }
+  
+  int i = 0;
+  int nNumber = (int) fNumber;
+  float fNumber_part = fNumber - nNumber;
+  sprintf(cString, "%d", nNumber);
+  do
+  {
+    nNumber = nNumber/10;
+    ++i;
+  }
+  while (nNumber > 0);
+  
+  if( nDigits > 0)
+  {
+    int nNumber_part;
+    cString[i++] = '.';
+    for( int digit = 0; digit < nDigits; ++digit)
+    {
+      fNumber_part = fNumber_part*10;
+      nNumber_part = fNumber_part;
+      fNumber_part = fNumber_part - nNumber_part;
+      
+      cString[i++] = ASCII_NUMBER + nNumber_part;
+      
+    }    
+    cString[i++] = '\0';
+  }
+  return cString;
+}
+
+float getCellLevel(float fVoltage)
 {
   float result;
-  float fIndex = (fVoltage - fBatteryLevelVoltageOffset) * nBatteryLevelVoltageMultiplier;
+  float fIndex = (fVoltage - fBatteryLevelVoltageOffset) * fBatteryLevelVoltageMultiplier;
   int index = fIndex;
   
-  if( index < 0 ) result = 0;
+  if( index < 0 ) result = -1;
   else if( index >= nMaxIndex) result = 1;
   else
   {
-    result = nBatteryLevel[index] + (nBatteryLevel[index+1] - nBatteryLevel[index]) * (fIndex-index);
+    result = fBatteryLevel[index] + (fBatteryLevel[index+1] - fBatteryLevel[index]) * (fIndex-index);
   }
   return result;
+}
+
+void mngLoopTime()
+{
+  t_loop_last = t_loop;
+  t_loop = millis() - t_startup;
+  //Serial.print("t_loop: ");
+  //Serial.print(t_loop);
+  //Serial.print("\n");
+  t_loopdelta = t_loop - t_loop_last;
+  Serial.print("t_loopdelta: ");
+  Serial.print(t_loopdelta);
+  Serial.print("\n");
+  if(t_loopdelta == 0) t_loopdelta_corrector = 0;
+  else t_loopdelta_corrector = SLEEP_TIME - t_loopdelta;
+  //Serial.print("t_loopdelta_corrector: ");
+  //Serial.print(t_loopdelta_corrector);
+  //Serial.print("\n");
+  
+  t_sleep_time = t_sleep_time + t_loopdelta_corrector;
+  if(t_sleep_time > 2 * SLEEP_TIME) t_sleep_time = 0;
+  //Serial.print("t_sleep_time: ");
+  //Serial.print(t_sleep_time);
+  //Serial.print("\n");
+}
+
+void wipeDisplayTop()
+{
+ lcd.setCursor(0, 0);
+ lcd.print("                ");
+}
+void wipeDisplayBottom()
+{
+ lcd.setCursor(0, 1);
+ lcd.print("                ");
+}
+
+void wipeDisplay()
+{
+  wipeDisplayTop();
+  wipeDisplayBottom();
+}
+
+void startCharging()
+{
+  digitalWrite(PIN_RELAIS, HIGH);
+}
+
+void stopCharging()
+{
+  digitalWrite(PIN_RELAIS, LOW);
+}
+
+bool battConnected()
+{
+  return fCellPercentage >= 0;
+}
+
+bool battCharged()
+{
+  return battConnected && fCellPercentage >= CELL_PERCENTAGE_MAX;
+}
+
+bool consumeButtonShortPress()
+{
+  bool consume = false;
+  if(bButtonPressed_short)
+  {
+    consume = !(bButtonPressed_short = false);
+  }
+  return consume;
+}
+
+bool consumeButtonLongPress()
+{
+  bool consume = false;
+  if(bButtonPressed_long)
+  {
+    consume = !(bButtonPressed_long = false);
+  }
+  return consume;
+}
+
+void mgnButton()
+{
+  bButtonPressed_last = bButtonPressed;
+  bButtonPressed = digitalRead(PIN_BUTTON) == 0;
+
+  if(bButtonPressed)
+  {
+    if(!bButtonPressed_last)
+    {
+      t_button_pressed = millis();
+    }
+  }
+  
+  if(!bButtonPressed)
+  {
+    if(bButtonPressed_last)
+    {
+       if((millis() - t_button_pressed) < BUTTON_LONGPRESS_TIME)
+       {
+          bButtonPressed_short = true;
+       }
+       else
+       {
+          bButtonPressed_long = true;
+       }       
+    }
+    if(!bButtonPressed_last)
+    {
+     if((millis() - t_button_pressed) > BUTTON_LONGPRESS_TIME*2)
+     {
+      consumeButtonShortPress();
+      consumeButtonLongPress();
+     }
+    }
+  }
+}
+
+bool switchState(state pState)
+{
+  bool stateSwitched = false;
+
+  switch(pState)
+    {
+      case STATE_INITIAL:
+        switch(gState)
+        {
+          case STATE_BATTERY_CONNECTED:
+          case STATE_BATTERY_CHARGING:
+          case STATE_BATTERY_CHARGED:
+          case STATE_BATTERY_ERROR:
+            gState = pState;
+            wipeDisplayTop();
+            lcd.setCursor(0, 0);
+            lcd.print(TEXT_CONNECT_BAT);
+            stopCharging();
+            break;
+          default:
+            break;
+        }
+        break;
+        
+      case STATE_BATTERY_CONNECTED:
+        switch(gState)
+        {
+          case STATE_INITIAL:
+          case STATE_BATTERY_CHARGING:
+          case STATE_BATTERY_CHARGED:
+            gState = pState;
+            wipeDisplayTop();
+            lcd.setCursor(0, 0);
+            lcd.print(TEXT_BAT_CONNECTED);
+            stopCharging();
+            break;
+          default:
+            break;
+        }
+        break;
+        
+      case STATE_BATTERY_CHARGING:
+        switch(gState)
+        {
+          case STATE_BATTERY_CONNECTED:
+            gState = pState;
+            wipeDisplayTop();
+            lcd.setCursor(0, 0);
+            lcd.print(TEXT_BAT_CHARGING);
+            startCharging();
+            break;
+          default:
+            break;
+        }
+        break;
+        
+      case STATE_BATTERY_CHARGED:
+        switch(gState)
+        {
+          case STATE_INITIAL:
+          case STATE_BATTERY_CONNECTED:
+            gState = pState;
+            wipeDisplayTop();
+            lcd.setCursor(0, 0);
+            lcd.print(TEXT_BAT_CHARGED);
+            stopCharging();
+            break;
+          default:
+            break;
+        }
+        break;
+        
+      case STATE_BATTERY_ERROR:
+        switch(gState)
+        {
+          case STATE_INITIAL:
+          case STATE_BATTERY_CONNECTED:
+          case STATE_BATTERY_CHARGING:
+          case STATE_BATTERY_CHARGED:
+            gState = pState;
+            wipeDisplayTop();
+            lcd.setCursor(0, 0);
+            lcd.print(TEXT_BAT_ERROR);
+            stopCharging();
+            break;
+          default:
+            break;
+        }
+        break;
+      default:
+        break;
+    }  
+    return stateSwitched;
 }
